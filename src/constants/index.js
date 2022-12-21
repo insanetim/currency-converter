d0a0c7079e8cc3aad17ace87c524b275f1dc1d48
insanetim <47120724+insanetim@users.noreply.github.com>
2022-12-21 21:00:17 +0200

    api update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
 export const API_KEY = 'fT84Tc5ym5W9eoWfmiq7wQGzmHJTaGDG'
-export const API_URL = 'https://api.apilayer.com/fixer'
+export const API_URL = 'https://api.apilayer.com/exchangerates_data'
